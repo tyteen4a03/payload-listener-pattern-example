fe0cff2056ce0ea0024cde81562518b265abc627
tyteen4a03 <tyteen4a03@thelastcode.io>
2025-01-23 01:47:01 +0100

    chore: pin dependencies, remove sharp

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,11 +1,9 @@
-// storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
-import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -29,9 +27,7 @@ export default buildConfig({
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
   }),
-  sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
   ],
 })
